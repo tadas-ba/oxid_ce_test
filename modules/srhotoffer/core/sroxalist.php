bd7bbac344bbe4f11a936474d9471a18b0c95299
tadas-ba <barauskas.tadas@gmail.com>
2013-01-28 09:34:33 +0200

    FIX: srhotoffer modules sroxalist
    Added a method that is supposed to check if an article has HotOffer tag.

--- modules/srhotoffer/core/sroxalist.php
+++ modules/srhotoffer/core/sroxalist.php
@@ -7,7 +7,21 @@
  * To change this template use File | Settings | File Templates.
  */
 
+
+/**
+ * Check's if an article has a HotOffer tag
+ * returns true if does, false otherwise
+ */
 class sroxaList extends aList
 {
-
+    public function isHotOffer($sOxId)
+    {
+        $oArticle = oxNew('oxarticle');
+        $oArticle = init('oxarticle');
+        $oArticle->load($sOxId);
+        if($oArticle->oxarticle__sroxhotoffer->value == '1'){
+            return true;
+        }
+        return false;
+    }
 }
